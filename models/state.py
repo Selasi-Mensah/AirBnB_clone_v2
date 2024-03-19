87428862401eb25388bcf19f7b45a6b6ee05285d
Selasi Agbenyegah Mensah <105177068+Selasi-Mensah@users.noreply.github.com>
2024-03-19 01:33:35 +0000

    MOdels Addition

--- models/state.py
+++ models/state.py
@@ -1,8 +1,36 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.city import City
+from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(
+        String(128),
+        nullable=False
+        )
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship(
+            "City",
+            backref="state",
+            cascade="all, delete"
+            )
+    else:
+        @property
+        def cities(self):
+            """
+            Getter attribute cities that returns the list of
+            City instances with state_id equals to the current State.id
+            """
+            list_cities = []
+            for city in models.storage.all(City).values():
+                if city.state_id == self.id:
+                    list_cities.append(city)
+            return list_cities
